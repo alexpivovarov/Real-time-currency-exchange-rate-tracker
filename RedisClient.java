5d0903e770e7b703c4e9cce560e70dabd5901cd0
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2025-08-11 21:53:31 +0500

    Update RedisClient.java

--- RedisClient.java
+++ RedisClient.java
@@ -1,22 +1,31 @@
 // communicates with Redis
 
+package com.example.exchange.util;
+
+import redis.clients.jedis.Jedis;
+
 public class RedisClient {
     private final Jedis jedis;
 
     public RedisClient() {
-        this.jedis = new Jedis("localhost", 6379)   // Connects to Redis on your machine (localhost: 6379)
+        String host = System.getenv("REDIS_HOST");
+        if (host == null || host.isBlank()) host = "localhost";
+        int port = 6379;
+        System.out.println("[REDIS] Connecting to " + host + ":" + port);
+        this.jedis = new Jedis(host, port);
     }
 
-    public void set(String key, String value) {
-        jedis.set(key, value);  // Saves key-value pair to Redis
+    public void setRate(String key, String value) {
+        jedis.set(key, value);
     }
 
-    public String get(String key) {
-        return jedis.get(key);  // Fetches a value from Redis by its key
+    public String getRate(String key) {
+        return jedis.get(key);
     }
 }
 
 // Every 5 minutes, your app calls a currency echange API, gets the latest USD-to-other-currency rates, and saves them into Redis like a key-value store
 // Example: 
 // "USD_EUR" -> "0.893"
+
 // "USD_GBP" -> "0.7814"
