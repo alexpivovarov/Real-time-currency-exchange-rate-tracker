53b80e7c6d88dd5be0344e6d121f083fa437de08
Alex Pivovarov <139866046+alexpivovarov@users.noreply.github.com>
2025-08-29 17:01:37 +0300

    Create build.gradle.kts

---
+++ quote-service/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("java")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
